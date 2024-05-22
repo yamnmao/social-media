d8a0cddd42be3a3e5bf07d5d758d4fef1cbc51e9
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-05-21 23:57:09 -0700

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,35 +3,48 @@ import {auth} from "../config/firebase"
 import {signOut} from "firebase/auth"
 import {useAuthState} from 'react-firebase-hooks/auth'
 
+
 export const Navbar = ()=>{
     const [user,loading, error] = useAuthState(auth);
+    //loading and error are part of the state returned by the useAuthState hook from the react-firebase-hooks
 
     const signUserOut = async()=>{
-        await signOut(auth);
+        try{
+            await signOut(auth);
+        }catch(err){
+            console.error("Error signing out:", err);
+        }
     }
     
     return (
     <div className = "navbar">
         <div className = "Links">
             <Link to="/">Home</Link>
-            {!user ? (<Link to="/login">Login</Link>):
-                    (<Link to="/createpost">Create Post</Link>)}
+            {!user ? (
+                <Link to="/login">Login</Link>
+                ):(
+                <Link to="/createpost">Create Post</Link>
+                )}
         </div>
         <div className="user">
-            { user && (
-                <>
-                <p>
-                    {auth.currentUser?.displayName}
-                </p>
-                    <img src={auth.currentUser?.photoURL || ""}/>
-                    {/**auth.currentUser?.photoURL: The ?. is the optional chaining operator in JavaScript (and by extension, TypeScript),
-                    * only access photoURL if currentUser is not null or undefined. If currentUser is null or undefined, it will return undefined.
-                    * || "": The logical OR operator || is used here to provide a fallback value. 
-                    * If auth.currentUser?.photoURL is undefined ( no user logged in or user has no photoURL), then the empty string "" is used as the src value. 
-                    * This prevents the browser from making a request to a non-existent URL, which could result in a 404 error.
-                    */
-                    }
-                    <button onClick={signUserOut}>Log Out</button>
+            { loading?(
+                <p>Loading...</p>
+            ):error?(
+                <p>Error:{error.message}</p>
+            ):user && (
+                    <>
+                    <p>
+                        {auth.currentUser?.displayName}
+                    </p>
+                        <img src={auth.currentUser?.photoURL || ""}/>
+                        {/**auth.currentUser?.photoURL: The ?. is the optional chaining operator in JavaScript (and by extension, TypeScript),
+                        * only access photoURL if currentUser is not null or undefined. If currentUser is null or undefined, it will return undefined.
+                        * || "": The logical OR operator || is used here to provide a fallback value. 
+                        * If auth.currentUser?.photoURL is undefined ( no user logged in or user has no photoURL), then the empty string "" is used as the src value. 
+                        * This prevents the browser from making a request to a non-existent URL, which could result in a 404 error.
+                        */
+                        }
+                        <button onClick={signUserOut}>Log Out</button>
                     </>
                 )
             }
